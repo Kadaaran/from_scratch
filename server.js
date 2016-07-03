3e24deaba286adb37f78aac5cbf0d35171a1e48c
Kassandre <kpedro@student.42.fr>
2016-07-03 17:39:07 +0200

    add script npm start

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var app = express();
+ 
+var compiler = webpack(webpackConfig);
+ 
+app.use(express.static(__dirname + '/output'));
+ 
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+ 
+var server = app.listen(3000, function() {
+  console.log('App launched at localhost:3000 🚀"' );
+});
